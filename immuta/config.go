959c4dba78e260e486d0f14da1e5c629d405e19b
Kieran Taylor <kieran.taylor@instacart.com>
2023-02-17 19:39:12 -0800

    add method to configure immuta client

--- immuta/config.go
+++ immuta/config.go
@@ -1,17 +1,31 @@
 package immuta
 
+import (
+	"fmt"
+	"github.com/instacart/terraform-provider-immuta/client"
+)
+
 type Config struct {
 	APIKey string
 	Host   string
 }
 
-func (config *Config) Client() (*ImmutaClient, error) {
+func (config *Config) ImmutaClient() (interface{}, error) {
+	if config.Host == "" {
+		return nil, fmt.Errorf("no Host set")
+	}
+	if config.APIKey == "" {
+		return nil, fmt.Errorf("no API Key set")
+	}
+
+	userAgent := "fmt.Sprintf(\"HashiCorp Terraform/%s (+https://www.terraform.io) Terraform Plugin SDK/%s\" )\n"
+
+	client := client.NewClient(config.APIKey, config.Host, userAgent)
+
 	//if err := config.validate(); err != nil {
 	//	return nil, err
 	//}
+	err := error(nil)
 
-	return &ImmutaClient{
-		APIKey: config.APIKey,
-		Host:   config.Host,
-	}, nil
+	return client, err
 }
